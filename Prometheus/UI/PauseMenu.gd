8bb6dd84d8dce985bf9ba2b19d0208f2e150028f
killercho <ggeorgi60@gmail.com>
2020-04-18 16:57:01 +0300

    New restart button

--- Prometheus/UI/PauseMenu.gd
+++ Prometheus/UI/PauseMenu.gd
@@ -6,3 +6,8 @@ func _ready():
 
 func _on_Resume_pressed():
 	get_tree().change_scene("res://Node2D.tscn")
+
+
+func _on_RestartLevel_pressed():
+	GlobalVars.playerSavedPosition = [100, 0]
+	get_tree().change_scene("res://Node2D.tscn")
