3279b43a6c624b019f1804b2ef343b8ed45da548
Shapkofil <ldshapkofil@gmail.com>
2020-04-19 14:01:47 +0300

    Walking Particles

---
+++ Prometheus/Durt.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var player
+var particles
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_parent()
+	particles = get_node("DurtParticles")
+	set_process(true)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if player.velocity.y == 0 and not player.velocity.x == 0:
+		particles.emitting = true
+		scale.x = max(min(player.velocity.x, 0.5),-0.5)
+	else:
+		particles.emitting = false
+	
