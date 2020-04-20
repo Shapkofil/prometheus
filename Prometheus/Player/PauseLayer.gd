ead4b94054e6ad7088153e21a01ad0062ff036e0
killercho <ggeorgi60@gmail.com>
2020-04-20 19:48:50 +0300

    Fixed some bugs

--- Prometheus/Player/PauseLayer.gd
+++ Prometheus/Player/PauseLayer.gd
@@ -1,5 +1,30 @@
 extends CanvasLayer
 
+var isPaused
 
 func _ready():
 	pass
+
+
+func _input(event):
+	if event.is_action_pressed("ui_pause"):
+		isPaused = not get_tree().paused
+		get_tree().paused = not get_tree().paused
+		get_child(0).visible = isPaused
+
+
+func _on_Resume_pressed():
+	get_tree().paused = not get_tree().paused
+	get_child(0).visible = not isPaused
+
+
+func _on_RestartLevel_pressed():
+	get_tree().paused = false
+# warning-ignore:return_value_discarded
+	get_tree().reload_current_scene()
+
+
+func _on_QuitToMain_pressed():
+	get_parent().go_back_to_menu()
+
+
